3a6971bb3dd1f687da1fa38c4cd9fc37b326a7de
deepaghera <deepaghera2000@gmail.com>
2020-02-21 01:12:05 +0530

    Second commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="jumbotron">
+            <h1>PluraSHite Administration</h1>  
+            <p>React,Flux, ANd React Router for ultra-responsive</p>        
+            <a href="/about"> About </a>                                                                                                                   
+        </div>
+    );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { render } from "react-dom";
+import App from './components/App';
+
+render(<App />,document.getElementById('root'));
