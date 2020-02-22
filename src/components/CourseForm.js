5d96d2ef7b8d6bdd4c9bacb160d6b8b65b1e7205
deepaghera <deepaghera2000@gmail.com>
2020-02-22 23:05:17 +0530

    form complete

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,21 +1,20 @@
 import React from "react";
+import TextInput from './common/TextInput';
+import PropTypes from 'prop-types';
 
 function CourseForm(props) {
   return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <div className="field">
-          <input
+    <form onSubmit={props.onSubmit}>
+     
+          <TextInput
             id="title"
-            type="text"
-            onChange={props.onTitleChange}
+            label="Title"
+            onChange={props.onChange}
             name="title"
-            className="form-control"
             value={props.course.title}
+            error={props.errors.title}
           />
-        </div>
-      </div>
+       
 
       <div className="form-group">
         <label htmlFor="author">Author</label>
@@ -23,32 +22,39 @@ function CourseForm(props) {
           <select
             id="author"
             name="authorId"
+            onChange={props.onChange}
             value={props.course.authorId || ""}
             className="form-control"
           >
             <option value="" />
             <option value="1">Cory House</option>
-            <option value="2">Scott Allen</option>
+            <option value="2">Scott Allen</option>                                         
           </select>
         </div>
+        {props.errors.authorId && 
+        (<div className="alert alert-danger">{props.errors.authorId}</div>)}
       </div>
 
-      <div className="form-group">
-        <label htmlFor="category">Category</label>
-        <div className="field">
-          <input
-            type="text"
+      
+          <TextInput
             id="category"
+            label="Category"
             name="category"
-            className="form-control"
+            onChange={props.onChange}
             value={props.course.category}
+            error={props.errors.category}
           />
-        </div>
-      </div>
-
+        
       <input type="submit" value="Save" className="btn btn-primary" />
     </form>
   );
 }
+
+  CourseForm.propTypes = {
+    course : PropTypes.object.isRequired,
+    onSubmint: PropTypes.func.isRequired,
+    onChange : PropTypes.func.isRequired,
+    errors : PropTypes.object.isRequired
+  }
 
 export default CourseForm;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,7 +1,10 @@
-import React,{ useState } from 'react';
+import React,{ useState , useEffect} from 'react';
 import CourseForm from './CourseForm';
+import * as courseApi from '../api/courseApi';
+import { toast } from 'react-toastify';
 // import { Prompt } from 'react-router-dom';
 const ManageCoursesPage = props => {
+    const [errors, setErrors] = useState({});
    const [course , setCourse] = useState({
         id:null,
         slug:"",
@@ -9,14 +12,43 @@ const ManageCoursesPage = props => {
         authorId:null,
         category:""
    });
-   function handleTitleChage(event){
-       const updatedCourse =  {...course,title:event.target.value};
+   useEffect ( () => {
+       const slug = props.match.params.slug;
+       if(slug){
+           courseApi.getCourseBySlug(slug).then( _course => setCourse(_course))
+       }
+   },[props.match.params.slug])
+   function handleChange(event){
+       const updatedCourse =  {...course,[event.target.name]:event.target.value};
        setCourse(updatedCourse);
+   }
+
+   function formIsValid(){
+       const _errors = {};
+       if(!course.title) _errors.title = "Title is Required";
+       if(!course.authorId) _errors.authorId = "Author Id is Required";
+       if(!course.category) _errors.category = "Category Id is Required";
+       setErrors(_errors);
+       return Object.keys(_errors).length === 0;
+
+   }
+
+   function handleSubmit(event){
+       event.preventDefault();
+       if(!formIsValid()) return;
+       courseApi.saveCourse(course).then(() => {                                                                        
+           props.history.push("/courses");
+           toast.success("Course Saved.");
+       });
    }
     return (
         <>
         <h2>Manage Course</h2>
-        <CourseForm course={course} onTitleChange={handleTitleChage}/>
+        <CourseForm course={course} 
+            errors={errors}
+            onChange={handleChange} 
+            onSubmit={handleSubmit}
+         />
         {props.match.params.slug}
         </>
     )
